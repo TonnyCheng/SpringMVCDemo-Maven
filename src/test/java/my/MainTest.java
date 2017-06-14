4e3dd7a06eaf03df5e2edb241698a8fc5cdd4fce
TonnyCheng <645733908@qq.com>
2017-06-14 16:49:26 +0800

    sec

---
+++ src/test/java/my/MainTest.java
@@ -0,0 +1,23 @@
+package my;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test1(){
+		System.out.println("11");
+	}
+	@Test
+	public void test2(){
+		System.out.println("11");
+	}
+	@Test
+	public void test3(){
+		System.out.println("11");
+	}
+	@Test
+	public void test4(){
+		System.out.println("11");
+	}
+}
